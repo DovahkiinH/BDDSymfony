6e0290ed8bbf631966fd641617d915a0b8029ee1
DovahkiinH <loick.caplier@gmail.com>
2024-03-06 16:17:33 +0100

    BDDSymfony

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class MonControlleurController extends AbstractController
+class AccueilController extends AbstractController
 {
-    #[Route('/mon/controlleur', name: 'app_mon_controlleur')]
+    #[Route('/accueil', name: 'app_accueil')]
     public function index(): Response
     {
-        return $this->render('mon_controlleur/index.html.twig', [
-            'controller_name' => 'MonControlleurController',
+        return $this->render('accueil/index.html.twig', [
+            'controller_name' => 'AccueilController',
         ]);
     }
 }
