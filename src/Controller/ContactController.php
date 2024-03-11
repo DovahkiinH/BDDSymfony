ec23cd7ffe000d87e9a2f7efcc8c80cab48529be
DovahkiinH <loick.caplier@gmail.com>
2024-03-11 15:15:15 +0100

    BDDSymfony

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,18 +2,39 @@
 
 namespace App\Controller;
 
+use App\Entity\Contact;
+use App\Form\DemoFormType;
 use App\Form\ContactFormType;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
-
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'app_contact')]
-    public function index(Request $request): Response
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
         $form = $this->createForm(ContactFormType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            //on crée une instance de Contact
+            $message = new Contact();
+            // Traitement des données du formulaire
+            $data = $form->getData();
+            //on stocke les données récupérées dans la variable $message
+            $message = $data;
+
+            $entityManager->persist($message);
+            $entityManager->flush();
+
+            // Redirection vers accueil
+            return $this->redirectToRoute('app_accueil');
+        }
+
 
         return $this->render('/contact/index.html.twig', [
             'form' => $form
--- src/Repository/ArtistRepository.php
+++ src/Repository/ArtistRepository.php
@@ -45,4 +45,6 @@ class ArtistRepository extends ServiceEntityRepository
     //            ->getOneOrNullResult()
     //        ;
     //    }
+
+    
 }
